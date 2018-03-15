262344a79b0d3980c3752378883f88a760bb3bdb
Laleh <laleh@Lilis-MacBook-Pro-2.local>
2018-03-14 17:14:06 -0700

    index.js

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-//Index File
+//  Define the required packages
+const 
+express = require('express'),
+app = express(),
+logger = require('morgan'),
+bodyParser = require('body-parser'),
+axios = require('axios'),
+httpClient = axios.create(),
+PORT = 3000
+
+
+//Middleware
+app.use(logger('dev'))
+app.use(bodyParser.json())
+app.use(express.static(`${__dirname}/public`))
+
+
+app.get('/', (req, res) => {
+    res.sendFile(`${__dirname}/public/index.html`)
+})
+
+
+//Set the PORT 3000
+app.listen(PORT, (err) => {
+    console.log(err || `Server running on ${PORT}.`)
+    })
